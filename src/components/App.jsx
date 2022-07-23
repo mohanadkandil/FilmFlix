949ce9d3109a968fdb127f8038fe7a686fadbfef
mohanadkandil <mohanadmkandil@gmail.com>
2022-07-23 13:46:07 +0200

    styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
-
+import useStyles from './styles';
 import { Movies, MovieInformation, Profile, NavBar, Actors } from './index';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <NavBar />
-    <main>
-      <Routes>
-        <Route exact path="/movies/:id" element={<MovieInformation />} />
-        <Route exact path="/actors/:id" element={<Actors />} />
-        <Route exact path="/" element={<Profile />} />
-        <Route exact path="/movies" element={<Movies />} />
-        <Route exact path="/profile/:id" element={<Profile />} />
-      </Routes>
-    </main>
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route exact path="/movies/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/" element={<Profile />} />
+          <Route exact path="/movies" element={<Movies />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
