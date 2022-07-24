67896f09d8ce4997bb5b020c7220e6d6040c9140
mohanadkandil <mohanadmkandil@gmail.com>
2022-07-24 21:27:42 +0200

    feat: add redux toolkit

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,3 +1,9 @@
-import React from 'react';
+import { Box, CircularProgress, Typography, useMediaQuery } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useGetMoviesQuery } from '../../services/TMDB';
 
-export const Movies = () => <div>Movies</div>;
+export const Movies = () => {
+  const { data } = useGetMoviesQuery();
+  console.log('🚀 ~ file: Movies.jsx ~ line 8 ~ Movies ~ data', data);
+};
